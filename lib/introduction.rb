716e3bc1e7dfc2844260f017bf975846519517be
Tami Reichold <reichold.tamara@gmail.com>
2019-09-09 16:43:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,18 @@
+def
+introduction(name)
+  puts "Hi, my name is #{name}."
+end;
+
+def
+introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end;
+
+def
+introduction_with_language_option(name, language="Ruby")
+end;
+
+
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
